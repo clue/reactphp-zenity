f38b2c55c82e2a8b2156c452f0cb6ff36588ac8c
Christian Lück <christian@lueck.tv>
2013-09-04 00:04:47 +0200

    Adjust to new blocking API

--- example/blocking.php
+++ example/blocking.php
@@ -11,15 +11,15 @@ $loop = Factory::create();
 $launcher = new Launcher($loop);
 $builder = new Builder($launcher);
 
-$name = $builder->entry('Search package')->wait($launcher);
+$name = $builder->entry('Search package')->waitReturn();
 if ($name === false) {
     exit;
 }
 var_dump($name);
 
-$builder->pulsateFor(3.0, 'Searching packagist.org for "' . $name . '"...')->wait($launcher);
+$builder->pulsateFor(3.0, 'Searching packagist.org for "' . $name . '"...')->wait();
 
 $packages = array('mink', 'behat', 'phpunit', 'box', 'boris');
-$pid = $builder->listRadio($packages, 'Select package')->wait($launcher);
+$pid = $builder->listRadio($packages, 'Select package')->wait();
 
 var_dump($packages[$pid]);
