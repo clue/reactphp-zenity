clue-labs/php-zenity-react

Adjust usage examples to use dedicated processors

example/dialog.php
@@ -11,16 +11,16 @@ $loop = Factory::create();
 $launcher = new Launcher($loop);
 $builder = new Builder($launcher);
 
-$builder->entry('What\'s your name?', getenv('USER'))->setTitle('Enter your name')->then(function ($name) use ($builder) {
-    $builder->info('Welcome to the introduction of zenity, ' . $name)->then(function () use ($builder) {
-        $builder->question('Do you want to quit?')->then(function () use ($builder) {
-            $builder->error('Oh noes!')->run();
+$builder->entry('What\'s your name?', getenv('USER'))->setTitle('Enter your name')->launch()->then(function ($name) use ($builder) {
+    $builder->info('Welcome to the introduction of zenity, ' . $name)->launch()->then(function () use ($builder) {
+        $builder->question('Do you want to quit?')->launch()->then(function () use ($builder) {
+            $builder->error('Oh noes!')->launch();
         }, function() use ($builder) {
-            $builder->warning('You should have selected yes!')->run();
+            $builder->warning('You should have selected yes!')->launch();
         });
     });
 }, function () use ($builder) {
-    $builder->warning('No name given')->run();
+    $builder->warning('No name given')->launch();
 });
 
 $loop->run();

example/form.php
@@ -21,9 +21,7 @@ $form->addCalendar('Birthday');
 $form->addEntry('Nickname');
 $form->addList('Gender', array('Male', 10, 'Female', 0), array('Gender', 'Number'));
 
-$form->run();
-
-$form->then(function($result) {
+$form->launch()->then(function($result) {
     var_dump('result', $result);
 }, function() {
     var_dump('form canceled');

example/menu.php
@@ -17,19 +17,19 @@ $main = function() use (&$main, $builder) {
     $menu->setWindowIcon('info');
     $menu->setTitle('Main menu');
 
-    $menu->then(function ($selected) use ($builder, $main) {
+    $menu->launch()->then(function ($selected) use ($builder, $main) {
         if ($selected === '0') {
             // U+2212 MINUS SIGN for alignment
-            $builder->listRadio(array('+2', '+1', '±0', '−1', '−2'), 'Introduction Level', 2)->then(function ($level) use ($main, $builder) {
-                $builder->info('Level ' . var_export($level, true))->then($main, $main);
+            $builder->listRadio(array('+2', '+1', '±0', '−1', '−2'), 'Introduction Level', 2)->launch()->then(function ($level) use ($main, $builder) {
+                $builder->info('Level ' . var_export($level, true))->launch()->then($main, $main);
             }, $main);
         } elseif ($selected === '1') {
-            $builder->listCheck(array('Unit', 'Functional', 'Acceptance (slow)'), 'Selected test suits to run', array(0, 1))->then(function ($tests) use ($main, $builder) {
-                $builder->info('Tests: ' . var_export($tests, true))->then($main, $main);
+            $builder->listCheck(array('Unit', 'Functional', 'Acceptance (slow)'), 'Selected test suits to run', array(0, 1))->launch()->then(function ($tests) use ($main, $builder) {
+                $builder->info('Tests: ' . var_export($tests, true))->launch()->then($main, $main);
             }, $main);
         } elseif ($selected === '2') {
-            $builder->confirmLicense(__DIR__ . '/../README.md', 'I have read the README.md file')->then(function ($checked) use ($main, $builder) {
-                $builder->info('Clicked ' . var_export($checked, true))->then($main, $main);
+            $builder->confirmLicense(__DIR__ . '/../README.md', 'I have read the README.md file')->launch()->then(function ($checked) use ($main, $builder) {
+                $builder->info('Clicked ' . var_export($checked, true))->launch()->then($main, $main);
             }, $main);
         } elseif ($selected === '3') {
             $builder->table(
@@ -42,12 +42,12 @@ $main = function() use (&$main, $builder) {
                     'Description',
                     'Status'
                 )
-            )->then(function ($bug) use ($main, $builder) {
-                $pulser = $builder->pulsate('Loading bug #' . $bug . '...' . PHP_EOL . '(This will not actually do anything...)');
+            )->launch()->then(function ($bug) use ($main, $builder) {
+                $pulser = $builder->pulsate('Loading bug #' . $bug . '...' . PHP_EOL . '(This will not actually do anything...)')->launch();
                 $pulser->then($main, $main);
             }, $main);
         } else {
-            $builder->info('Selected ' . var_export($selected, true))->then($main, $main);
+            $builder->info('Selected ' . var_export($selected, true))->launch()->then($main, $main);
         }
     });
 };

example/progress.php
@@ -12,8 +12,7 @@ $loop = Factory::create();
 $launcher = new Launcher($loop);
 $builder = new Builder($launcher);
 
-$progress = $builder->progress('Pseudo-processing...');
-$progress->run();
+$progress = $builder->progress('Pseudo-processing...')->launch();
 
 $loop->addPeriodicTimer(0.1, function ($timer) use ($progress) {
     $progress->advance(mt_rand(0, 3));
@@ -23,8 +22,7 @@ $loop->addPeriodicTimer(0.1, function ($timer) use ($progress) {
     }
 });
 
-$pulsate = $builder->pulsate('[1/3] Preparing...');
-$pulsate->run();
+$pulsate = $builder->pulsate('[1/3] Preparing...')->launch();
 
 $loop->addTimer(2, function() use ($pulsate) {
     $pulsate->setText('[2/3] Downloading...');
@@ -38,7 +36,7 @@ $loop->addTimer(6, function() use ($pulsate) {
     $pulsate->complete();
 });
 
-$builder->info('Quit "Processing"?')->then(function () use ($pulsate) {
+$builder->info('Quit "Processing"?')->launch()->then(function () use ($pulsate) {
     $pulsate->close();
 });
 

clue/reactphp-zenity

example/dialog.php
@@ -11,9 +11,9 @@ $loop = Factory::create();
 $launcher = new Launcher($loop);
 $builder = new Builder($launcher);
 
-$builder->entry('What\'s your name?', getenv('USER'))->setTitle('Enter your name')->then(function ($name) use ($builder) {
-    $builder->info('Welcome to the introduction of zenity, ' . $name)->then(function () use ($builder) {
-        $builder->question('Do you want to quit?')->then(function () use ($builder) {
+$builder->entry('What\'s your name?', getenv('USER'))->setTitle('Enter your name')->run()->then(function ($name) use ($builder) {
+    $builder->info('Welcome to the introduction of zenity, ' . $name)->run()->then(function () use ($builder) {
+        $builder->question('Do you want to quit?')->run()->then(function () use ($builder) {
             $builder->error('Oh noes!')->run();
         }, function() use ($builder) {
             $builder->warning('You should have selected yes!')->run();

example/form.php
@@ -20,9 +20,7 @@ $form->addCalendar('Birthday');
 $form->addEntry('Nickname');
 $form->addList('Gender', array('Male', 10, 'Female', 0), array('Gender', 'Number'));
 
-$form->run();
-
-$form->then(function($result) {
+$form->run()->then(function($result) {
     var_dump('result', $result);
 }, function() {
     var_dump('form canceled');

example/menu.php
@@ -16,19 +16,19 @@ $main = function() use (&$main, $builder) {
     $menu->setWindowIcon('info');
     $menu->setTitle('Main menu');
 
-    $menu->then(function ($selected) use ($builder, $main) {
+    $menu->run()->then(function ($selected) use ($builder, $main) {
         if ($selected === '0') {
             // U+2212 MINUS SIGN for alignment
-            $builder->listRadio(array('+2', '+1', '±0', '−1', '−2'), 'Introduction Level', 2)->then(function ($level) use ($main, $builder) {
-                $builder->info('Level ' . var_export($level, true))->then($main, $main);
+            $builder->listRadio(array('+2', '+1', '±0', '−1', '−2'), 'Introduction Level', 2)->run()->then(function ($level) use ($main, $builder) {
+                $builder->info('Level ' . var_export($level, true))->run()->then($main, $main);
             }, $main);
         } elseif ($selected === '1') {
-            $builder->listCheck(array('Unit', 'Functional', 'Acceptance (slow)'), 'Selected test suits to run', array(0, 1))->then(function ($tests) use ($main, $builder) {
-                $builder->info('Tests: ' . var_export($tests, true))->then($main, $main);
+            $builder->listCheck(array('Unit', 'Functional', 'Acceptance (slow)'), 'Selected test suits to run', array(0, 1))->run()->then(function ($tests) use ($main, $builder) {
+                $builder->info('Tests: ' . var_export($tests, true))->run()->then($main, $main);
             }, $main);
         } elseif ($selected === '2') {
-            $builder->confirmLicense(__DIR__ . '/../README.md', 'I have read the README.md file')->then(function ($checked) use ($main, $builder) {
-                $builder->info('Clicked ' . var_export($checked, true))->then($main, $main);
+            $builder->confirmLicense(__DIR__ . '/../README.md', 'I have read the README.md file')->run()->then(function ($checked) use ($main, $builder) {
+                $builder->info('Clicked ' . var_export($checked, true))->run()->then($main, $main);
             }, $main);
         } elseif ($selected === '3') {
             $builder->table(
@@ -41,12 +41,12 @@ $main = function() use (&$main, $builder) {
                     'Description',
                     'Status'
                 )
-            )->then(function ($bug) use ($main, $builder) {
-                $pulser = $builder->pulsate('Loading bug #' . $bug . '...' . PHP_EOL . '(This will not actually do anything...)');
+            )->run()->then(function ($bug) use ($main, $builder) {
+                $pulser = $builder->pulsate('Loading bug #' . $bug . '...' . PHP_EOL . '(This will not actually do anything...)')->run();
                 $pulser->then($main, $main);
             }, $main);
         } else {
-            $builder->info('Selected ' . var_export($selected, true))->then($main, $main);
+            $builder->info('Selected ' . var_export($selected, true))->run()->then($main, $main);
         }
     });
 };

example/progress.php
@@ -11,8 +11,7 @@ $loop = Factory::create();
 $launcher = new Launcher($loop);
 $builder = new Builder($launcher);
 
-$progress = $builder->progress('Pseudo-processing...');
-$progress->run();
+$progress = $builder->progress('Pseudo-processing...')->run();
 
 $loop->addPeriodicTimer(0.1, function ($timer) use ($progress) {
     $progress->advance(mt_rand(0, 3));
@@ -22,8 +21,7 @@ $loop->addPeriodicTimer(0.1, function ($timer) use ($progress) {
     }
 });
 
-$pulsate = $builder->pulsate('[1/3] Preparing...');
-$pulsate->run();
+$pulsate = $builder->pulsate('[1/3] Preparing...')->run();
 
 $loop->addTimer(2, function() use ($pulsate) {
     $pulsate->setText('[2/3] Downloading...');
@@ -37,7 +35,7 @@ $loop->addTimer(6, function() use ($pulsate) {
     $pulsate->complete();
 });
 
-$builder->info('Quit "Processing"?')->then(function () use ($pulsate) {
+$builder->info('Quit "Processing"?')->run()->then(function () use ($pulsate) {
     $pulsate->close();
 });
 