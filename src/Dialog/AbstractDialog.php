clue-labs/php-zenity-react

Rename abstract Zenity class to Dialog\AbstractDialog

src/Clue/Zenity/React/Dialog/AbstractDialog.php
@@ -1,12 +1,12 @@
 <?php
 
-namespace Clue\Zenity\React;
+namespace Clue\Zenity\React\Dialog;
 
 use Clue\Zenity\React\Launcher;
 use React\Promise\PromiseInterface;
 use React\Promise\Deferred;
 
-abstract class Zenity implements PromiseInterface
+abstract class AbstractDialog implements PromiseInterface
 {
     private $launcher;
     private $deferred;

src/Clue/Zenity/React/Dialog/Calendar.php
@@ -2,9 +2,9 @@
 
 namespace Clue\Zenity\React\Dialog;
 
-use Clue\Zenity\React\Zenity;
+use Clue\Zenity\React\Dialog\AbstractDialog;
 
-class Calendar extends Zenity
+class Calendar extends AbstractDialog
 {
     protected $text;
     protected $year;

src/Clue/Zenity/React/Dialog/ColorSelection.php
@@ -2,9 +2,9 @@
 
 namespace Clue\Zenity\React\Dialog;
 
-use Clue\Zenity\React\Zenity;
+use Clue\Zenity\React\Dialog\AbstractDialog;
 
-class ColorSelection extends Zenity
+class ColorSelection extends AbstractDialog
 {
     protected $color;
     protected $showPalette = false;

src/Clue/Zenity/React/Dialog/FileSelection.php
@@ -2,9 +2,9 @@
 
 namespace Clue\Zenity\React\Dialog;
 
-use Clue\Zenity\React\Zenity;
+use Clue\Zenity\React\Dialog\AbstractDialog;
 
-class FileSelection extends Zenity
+class FileSelection extends AbstractDialog
 {
     protected $filename;
     protected $multiple = false;

src/Clue/Zenity/React/Dialog/Forms.php
@@ -2,9 +2,9 @@
 
 namespace Clue\Zenity\React\Dialog;
 
-use Clue\Zenity\React\Zenity;
+use Clue\Zenity\React\Dialog\AbstractDialog;
 
-class Forms extends Zenity
+class Forms extends AbstractDialog
 {
     protected $fields = array();
 

src/Clue/Zenity/React/Dialog/Info.php
@@ -2,9 +2,9 @@
 
 namespace Clue\Zenity\React\Dialog;
 
-use Clue\Zenity\React\Zenity;
+use Clue\Zenity\React\Dialog\AbstractDialog;
 
-class Info extends Zenity
+class Info extends AbstractDialog
 {
     protected $text;
 

src/Clue/Zenity/React/Dialog/Listing.php
@@ -2,14 +2,14 @@
 
 namespace Clue\Zenity\React\Dialog;
 
-use Clue\Zenity\React\Zenity;
+use Clue\Zenity\React\Dialog\AbstractDialog;
 
 /**
  *
  *
  * The name 'list' is a reserved keyword in PHP, hence the name 'listing'.
  */
-class Listing extends Zenity
+class Listing extends AbstractDialog
 {
     protected $text;
     protected $checklist = false;

src/Clue/Zenity/React/Dialog/TextInfo.php
@@ -2,9 +2,9 @@
 
 namespace Clue\Zenity\React\Dialog;
 
-use Clue\Zenity\React\Zenity;
+use Clue\Zenity\React\Dialog\AbstractDialog;
 
-class TextInfo extends Zenity
+class TextInfo extends AbstractDialog
 {
     protected $filename;
 

clue/reactphp-zenity

src/Dialog/AbstractDialog.php
@@ -1,12 +1,12 @@
 <?php
 
-namespace Clue\React\Zenity;
+namespace Clue\React\Zenity\Dialog;
 
 use Clue\React\Zenity\Launcher;
 use React\Promise\PromiseInterface;
 use React\Promise\Deferred;
 
-abstract class Zenity implements PromiseInterface
+abstract class AbstractDialog implements PromiseInterface
 {
     private $launcher;
     private $deferred;

src/Dialog/Calendar.php
@@ -2,9 +2,9 @@
 
 namespace Clue\React\Zenity\Dialog;
 
-use Clue\React\Zenity\Zenity;
+use Clue\React\Zenity\Dialog\AbstractDialog;
 
-class Calendar extends Zenity
+class Calendar extends AbstractDialog
 {
     protected $text;
     protected $year;

src/Dialog/ColorSelection.php
@@ -2,9 +2,9 @@
 
 namespace Clue\React\Zenity\Dialog;
 
-use Clue\React\Zenity\Zenity;
+use Clue\React\Zenity\Dialog\AbstractDialog;
 
-class ColorSelection extends Zenity
+class ColorSelection extends AbstractDialog
 {
     protected $color;
     protected $showPalette = false;

src/Dialog/FileSelection.php
@@ -2,9 +2,9 @@
 
 namespace Clue\React\Zenity\Dialog;
 
-use Clue\React\Zenity\Zenity;
+use Clue\React\Zenity\Dialog\AbstractDialog;
 
-class FileSelection extends Zenity
+class FileSelection extends AbstractDialog
 {
     protected $filename;
     protected $multiple = false;

src/Dialog/Forms.php
@@ -2,9 +2,9 @@
 
 namespace Clue\React\Zenity\Dialog;
 
-use Clue\React\Zenity\Zenity;
+use Clue\React\Zenity\Dialog\AbstractDialog;
 
-class Forms extends Zenity
+class Forms extends AbstractDialog
 {
     protected $fields = array();
 

src/Dialog/Info.php
@@ -2,9 +2,9 @@
 
 namespace Clue\React\Zenity\Dialog;
 
-use Clue\React\Zenity\Zenity;
+use Clue\React\Zenity\Dialog\AbstractDialog;
 
-class Info extends Zenity
+class Info extends AbstractDialog
 {
     protected $text;
 

src/Dialog/Listing.php
@@ -2,14 +2,14 @@
 
 namespace Clue\React\Zenity\Dialog;
 
-use Clue\React\Zenity\Zenity;
+use Clue\React\Zenity\Dialog\AbstractDialog;
 
 /**
  *
  *
  * The name 'list' is a reserved keyword in PHP, hence the name 'listing'.
  */
-class Listing extends Zenity
+class Listing extends AbstractDialog
 {
     protected $text;
     protected $checklist = false;

src/Dialog/TextInfo.php
@@ -2,9 +2,9 @@
 
 namespace Clue\React\Zenity\Dialog;
 
-use Clue\React\Zenity\Zenity;
+use Clue\React\Zenity\Dialog\AbstractDialog;
 
-class TextInfo extends Zenity
+class TextInfo extends AbstractDialog
 {
     protected $filename;
 