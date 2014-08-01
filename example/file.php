clue-labs/php-zenity-react

Rename Model namespace to Dialog

example/file.php
@@ -2,7 +2,7 @@
 
 use React\EventLoop\Factory;
 use Clue\Zenity\React\Launcher;
-use Clue\Zenity\React\Model\FileSelection;
+use Clue\Zenity\React\Dialog\FileSelection;
 use Clue\Zenity\React\Builder;
 
 require __DIR__ . '/../vendor/autoload.php';

example/form.php
@@ -2,8 +2,8 @@
 
 use React\EventLoop\Factory;
 use Clue\Zenity\React\Launcher;
-use Clue\Zenity\React\Model\FileSelection;
-use Clue\Zenity\React\Model\Forms;
+use Clue\Zenity\React\Dialog\FileSelection;
+use Clue\Zenity\React\Dialog\Forms;
 
 require __DIR__ . '/../vendor/autoload.php';
 

src/Clue/Zenity/React/Dialog/Calendar.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Clue\Zenity\React\Model;
+namespace Clue\Zenity\React\Dialog;
 
 use Clue\Zenity\React\Zenity;
 

src/Clue/Zenity/React/Dialog/Error.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace Clue\Zenity\React\Model;
+namespace Clue\Zenity\React\Dialog;
 
 use Clue\Zenity\React\Zenity;
 
-class Question extends Zenity
+class Error extends Zenity
 {
     protected $text;
 

src/Clue/Zenity/React/Dialog/Question.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace Clue\Zenity\React\Model;
+namespace Clue\Zenity\React\Dialog;
 
 use Clue\Zenity\React\Zenity;
 
-class Warning extends Zenity
+class Question extends Zenity
 {
     protected $text;
 

src/Clue/Zenity/React/Dialog/Scale.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace Clue\Zenity\React\Model;
+namespace Clue\Zenity\React\Dialog;
 
 use Clue\Zenity\React\Zenity;
 
-class Error extends Zenity
+class Info extends Zenity
 {
     protected $text;
 

src/Clue/Zenity/React/Dialog/Warning.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace Clue\Zenity\React\Model;
+namespace Clue\Zenity\React\Dialog;
 
 use Clue\Zenity\React\Zenity;
 
-class Info extends Zenity
+class Warning extends Zenity
 {
     protected $text;
 

clue/reactphp-zenity

example/file.php
@@ -2,7 +2,7 @@
 
 use React\EventLoop\Factory;
 use Clue\React\Zenity\Launcher;
-use Clue\React\Zenity\Model\FileSelection;
+use Clue\React\Zenity\Dialog\FileSelection;
 use Clue\React\Zenity\Builder;
 
 require __DIR__ . '/../vendor/autoload.php';

example/form.php
@@ -2,8 +2,8 @@
 
 use React\EventLoop\Factory;
 use Clue\React\Zenity\Launcher;
-use Clue\React\Zenity\Model\FileSelection;
-use Clue\React\Zenity\Model\Forms;
+use Clue\React\Zenity\Dialog\FileSelection;
+use Clue\React\Zenity\Dialog\Forms;
 
 require __DIR__ . '/../vendor/autoload.php';
 

src/Dialog/Calendar.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Clue\React\Zenity\Model;
+namespace Clue\React\Zenity\Dialog;
 
 use Clue\React\Zenity\Zenity;
 

src/Dialog/Error.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace Clue\React\Zenity\Model;
+namespace Clue\React\Zenity\Dialog;
 
 use Clue\React\Zenity\Zenity;
 
-class Question extends Zenity
+class Error extends Zenity
 {
     protected $text;
 

src/Dialog/Question.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace Clue\React\Zenity\Model;
+namespace Clue\React\Zenity\Dialog;
 
 use Clue\React\Zenity\Zenity;
 
-class Info extends Zenity
+class Question extends Zenity
 {
     protected $text;
 

src/Dialog/Scale.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace Clue\React\Zenity\Model;
+namespace Clue\React\Zenity\Dialog;
 
 use Clue\React\Zenity\Zenity;
 
-class Warning extends Zenity
+class Info extends Zenity
 {
     protected $text;
 

src/Dialog/Warning.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace Clue\React\Zenity\Model;
+namespace Clue\React\Zenity\Dialog;
 
 use Clue\React\Zenity\Zenity;
 
-class Info extends Zenity
+class Warning extends Zenity
 {
     protected $text;
 