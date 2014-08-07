4e03561db1e4de4d2e32d7fa4c49e30e82097d99
Christian Lück <christian@lueck.tv>
2014-08-08 01:14:10 +0200

    Add dedicated Progress dialog processor

---
+++ tests/Zen/ProgressZenTest.php
@@ -0,0 +1,28 @@
+<?php
+
+use Clue\React\Zenity\Zen\ProgressZen;
+
+class ProgressZenTest extends BaseZenTest
+{
+    public function testPercentage()
+    {
+        $zen = new ProgressZen($this->deferred, $this->process, 0);
+
+        $this->assertEquals(0, $zen->getPercentage());
+
+        $this->assertSame($zen, $zen->setPercentage(20));
+        $this->assertEquals(20, $zen->getPercentage());
+
+        $this->assertSame($zen, $zen->setText('hello'));
+
+        $this->assertSame($zen, $zen->advance(30));
+        $this->assertEquals(50, $zen->getPercentage());
+
+        $this->assertSame($zen, $zen->setText('world'));
+
+        $this->assertSame($zen, $zen->complete());
+        $this->assertEquals(100, $zen->getPercentage());
+
+        $this->assertEquals('20' . PHP_EOL . '#hello' . PHP_EOL . '50' . PHP_EOL . '#world' . PHP_EOL . '100' . PHP_EOL, $this->stdin);
+    }
+}
